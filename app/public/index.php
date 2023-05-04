fc087065e696384419c0498615f92ec4c905c35a
ToniMoldovan <antonio.moldovan82@yahoo.com>
2023-05-04 23:00:20 +0300

    added index.php inside /public directory

--- app/public/index.php
+++ app/public/index.php
@@ -1,2 +1,20 @@
 <?php
-echo 'Hello Worlds!';
+// always load composer autoload
+use App\controller\HomeController;
+use App\controller\MessageController;
+use App\core\Router;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$router = new Router();
+
+/* Routes */
+$router->addRoute('/', [HomeController::class, 'index']);
+$router->addRoute('/register', [HomeController::class, 'register']);
+$router->addRoute('/send-message', [MessageController::class, 'sendMessage']);
+
+
+// Dispatch the request
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$router->dispatch($requestUri, $requestMethod);
